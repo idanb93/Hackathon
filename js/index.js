8bd7d95909699adc9e1216b03c6fcd896fe73ff6
idanb93 <idanbar93@gmail.com>
2022-03-31 16:02:04 +0300

    index.html features

---
+++ js/index.js
@@ -0,0 +1,67 @@
+
+let topics = {
+
+    'React.js': 9,
+    'React-Hooks' : 2,
+    'Vue.js': 6,
+    'Node.js': 5,
+    'Python': 11,
+    'CSS Specificity': 2,
+    'CSS Flex-box': 3,
+    'Promise': 2,
+    'Docker': 3,
+    'Git': 1,
+    'Webpack': 2,
+
+}
+
+let topicsContainer = document.querySelector('.topics-container');
+let mousedown = false;
+let topicsKeys = Object.keys(topics);
+
+// populate topics container
+
+topicsKeys.forEach((key)=>{
+
+    let newDiv = document.createElement('div');
+    newDiv.style.padding = '15px 15px 15px 15px';
+    newDiv.style.textAlign = 'left';
+    newDiv.style.display = 'flex';
+    newDiv.style.flexDirection = 'row';
+    newDiv.style.justifyContent = 'space-between';
+    
+
+    let counterDiv = document.createElement('div');
+    counterDiv.style.height = '23px';
+    counterDiv.style.width = '23px';
+    counterDiv.style.borderRadius = '20px';
+    counterDiv.style.backgroundColor = 'black';
+    counterDiv.style.color = 'white';
+    counterDiv.style.textAlign = 'center';
+    counterDiv.textContent = topics[key];
+
+    let newAnchor = document.createElement('a');
+    newAnchor.href = '#';
+    newAnchor.style.textDecoration = 'none';
+    newAnchor.style.color = 'black';
+    newAnchor.style.fontSize = '18px';
+    newAnchor.style.textAlign = 'center';
+    newAnchor.textContent = key;
+    
+
+    newDiv.addEventListener('mouseover', function(){
+        newDiv.style.backgroundColor = 'lightgrey';
+    })
+
+    newDiv.addEventListener('mouseout', function(){
+        newDiv.style.backgroundColor = 'white';
+    })
+
+    newDiv.appendChild(newAnchor);
+    newDiv.appendChild(counterDiv);
+
+    topicsContainer.appendChild(newDiv);
+});
+
+// populate articles container
+
