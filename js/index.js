1669fd41816a3a4a31673cf79dbdd4899eb640e8
idanb93 <idanbar93@gmail.com>
2022-04-03 16:52:34 +0300

    fixed a small issue with the login page

--- js/index.js
+++ js/index.js
@@ -1,14 +1,4 @@
 
-// class Database {
-//     constructor() {
-//         this.userDetails = {};
-//     }
-// }
-
-// let database = new Database();
-
-// let usersDetails = {};
-
 let numOfUsers = 1;
 
 function registrationSuccessful(event) {
@@ -32,15 +22,8 @@ function registrationSuccessful(event) {
     document.querySelector('.username-input').value = '';
     document.querySelector('.password-input').value = '';
 
-    // database.userDetails[username] = password;
-    // console.log(database.userDetails);
-
-    // window.location.href="..\\html\\index.html";
-
 }
 function authenticate(event){
-
-    console.log(numOfUsers);
 
     event.preventDefault();
     
@@ -59,26 +42,9 @@ function authenticate(event){
     
         if (usernameFromLocalStorage === username && passwordFromLocalStorage === password){
             window.location.href="..\\html\\main-page.html";
-        } else {
-            alert('You entered incorrect username or password');
         }
     }
 
-    // console.log(database.userDetails);
-
-    // let usersDetailsKeys = Object.keys(usersDetails);
-
-    // usersDetailsKeys.forEach( (key)=>{
-
-    //     console.log(key);
-
-    //     if ( (key === username) && (usersDetails[key] === password) ){
-    //         window.open('..\\html\\main-page.html');
-    //     } else {
-    //         alert('You entered incorrect username or password');
-    //     }
-    // });
-
-    // alert('No such username or password');
+    alert('No such username or password');
 
 }
